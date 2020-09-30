9aaf7b92d9b5b8995c7003a4e87e001b12e0bf42
samiksha Shende <shende.samiksha@rediffmail.com>
2020-09-30 18:40:41 +0530

    Updated : Store and disaplay daily wage and total wage in dictionary code

--- wagecomputation.sh
+++ wagecomputation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 echo "Welcome to Employee Wage Computation Program"
 
-Wage=()
+declare -A Wage
 
 NUM_OF_WORKING_DAYS=20
 WAGE_PER_HR=20
@@ -13,22 +13,24 @@ monthly_Wage=0
 
 for i in `seq $NUM_OF_WORKING_DAYS`
 do
+
 	random=$(( $RANDOM % 3 ))
 
 
         case $random in
-        
-	1)
+
+
+        1)
                 #Present - FullDay
                 daily_Wage=$(( $WAGE_PER_HR * $FULL_DAY_HRS ))
         ;;
-        
-	2)
+
+        2)
                 #Present - HalfDay
                 daily_Wage=$(( $WAGE_PER_HR * $PART_TIME_HRS ))
         ;;
-        
-	*)
+
+        *)
                 #Absent
 		daily_Wage=0
         ;;
@@ -41,8 +43,9 @@ monthly_Wage=$(( $monthly_Wage + $daily_Wage ))
 
 done
 
-	Wage[$(( $i + 1))]=$monthly_Wage
+	Wage[$(( $i + 1 ))]=$monthly_Wage
 
-echo ${!Wage[@]}
-
-echo ${Wage[@]}
+for i in `seq 21`
+do
+	echo "$i - ${Wage[$i]}"
+done
