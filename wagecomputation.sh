9f5e6716ff87480041fc7b85373ab1400c671b40
samiksha Shende <shende.samiksha@rediffmail.com>
2020-09-30 16:24:44 +0530

    Added : Attendance code added

--- wagecomputation.sh
+++ wagecomputation.sh
@@ -1,2 +1,13 @@
 #!/bin/bash/
 echo "Welcome to Employee Wage Computation Program"
+
+echo "Attendance"
+
+	random=$(( $RANDOM % 2 ))
+
+	if [ $R1 -eq 0 ]
+	then
+		echo "Absent"
+	else
+		echo "Present"
+fi
